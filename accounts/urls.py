e659d374e6cd3e79af0869d9d0b28725a3d3ab20
paulBit3 <paultechnologie@gmail.com>
2020-07-28 20:32:13 -0500

    Implemented password change view and template

--- accounts/urls.py
+++ accounts/urls.py
@@ -33,4 +33,11 @@ urlpatterns = [
         template_name= 'accounts/password_reset_complete.html'), 
         name='password_reset_complete'),
 
+    # password change URLs
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name= 'accounts/password_change_done.html'), name='password_change_done'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(
+        template_name= 'accounts/password_change.html'), 
+        name='password_change'),
+
 ]
