2c5dd9431dfc4e9647c264b6a3a0dbf90d48cdee
paulBit3 <paultechnologie@gmail.com>
2020-08-25 21:31:08 -0500

    Change Password and Password Reset tested successfully

--- accounts/urls.py
+++ accounts/urls.py
@@ -22,17 +22,16 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
 
     # password reset URLs
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        success_url=reverse_lazy('accounts:password_reset_complete')
-        ), name='password_reset_confirm'),
 
     path('password-reset/', auth_views.PasswordResetView.as_view(
         success_url=reverse_lazy('accounts:password_reset_done')
         ), name='password_reset'),
-    
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name= 'registration/password_reset_done.html'), name='password_reset_done'),
-
+    
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('accounts:password_reset_complete')
+        ), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name= 'registration/password_reset_complete.html'), 
         name='password_reset_complete'),
@@ -41,8 +40,8 @@ urlpatterns = [
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(
         template_name= 'registration/password_change_done.html'), name='password_change_done'),
     path('password-change/', auth_views.PasswordChangeView.as_view(
-        template_name= 'registration/password_change_form.html',
-        success_url=reverse_lazy('accounts:password_change_done')), 
+        success_url=reverse_lazy('accounts:password_change_done'),
+        template_name= 'registration/password_change_form.html'), 
         name='password_change'),
     
 
